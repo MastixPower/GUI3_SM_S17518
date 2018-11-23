f548d36b48ca6e0dddb50cc3a00de0172b35d0de
MastixPower <mastixpower98@gmail.com>
2018-11-23 21:29:55 +0100

    Poczatkowa wersja Letters

--- src/zad1/Letters.java
+++ src/zad1/Letters.java
@@ -1,11 +1,40 @@
 package zad1;
+import java.util.Iterator;
 
-public class Letters {
+public class Letters extends Thread implements Iterable<String>{
 String word;
-
+String letter="Thread ";
+int counter=0;
+Iterator<String> iterator;
 Letters(String var)
 {
-	
+	this.word = var;
+    iterator = new Iterator<String>() {
+    	 @Override
+         public boolean hasNext() {
+    		 if(counter<word.length())return true;
+             return false;
+         }
+
+         @Override
+         public String next() {
+           letter=letter+Letters.this.word.charAt(counter);
+           counter++;
+             return Letters.this.letter;
+         }
+    };
+}
+Iterator<String> getThreads()
+{
+	return letter;
 }
 
+
+@Override
+public Iterator<String> iterator() {
+
+    return this.iterator;
+
+
+}
 }
